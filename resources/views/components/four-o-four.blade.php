fbdf2efb85580162a1a9eb71139423012b565635
Akbar <akbar@erip.me>
2020-06-06 04:09:33 +0500

    Added 404 page

---
+++ resources/views/components/four-o-four.blade.php
@@ -0,0 +1,10 @@
+<div class="saasbox-error-area section-padding-0-120">
+  <div class="container">
+    <div class="row justify-content-center align-items-center">
+      <div class="col-12 col-sm-10 col-md-9 col-lg-7"><img class="mb-5" src="img/bg-img/hero-7.png" alt="">
+        <h5>Oops! Page not found</h5>
+        <p>We couldn't find any results for your search. Try again.</p><a class="btn saasbox-btn mt-4" href="/">Back Home</a>
+      </div>
+    </div>
+  </div>
+</div>
